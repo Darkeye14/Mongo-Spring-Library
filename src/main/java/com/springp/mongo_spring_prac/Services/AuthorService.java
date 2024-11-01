43a714ac9a3d541ef3648a8eda0627f1cee72408
Darkeye14 <gyanopcon@gmail.com>
2024-11-01 23:37:53 +0530

    added files

--- src/main/java/com/springp/mongo_spring_prac/Services/AuthorService.java
+++ src/main/java/com/springp/mongo_spring_prac/Services/AuthorService.java
@@ -1,13 +1,13 @@
 package com.springp.mongo_spring_prac.Services;
 
 import com.springp.mongo_spring_prac.domain.documents.Author;
-
+import com.springp.mongo_spring_prac.domain.CreateUpdateAuthorRequest;
 import java.util.List;
 import java.util.Optional;
 
 public interface AuthorService {
     List<Author> listAuthors();
     Optional<Author> getAuthorById(String id);
-    Author createUpdateAuthor(String authorId, CreateUpdateauthorRequest createUpdateauthorRequest );
+    Author createUpdateAuthor(String authorId,CreateUpdateAuthorRequest createUpdateauthorRequest );
     void deleteAuthor(String id);
 }
