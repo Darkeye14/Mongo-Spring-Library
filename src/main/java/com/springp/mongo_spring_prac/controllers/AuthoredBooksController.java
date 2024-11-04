723816ee7fa7e7e75374407eea58a16fc274b02d
Darkeye14 <gyanopcon@gmail.com>
2024-11-04 23:25:18 +0530

    added more functions

--- src/main/java/com/springp/mongo_spring_prac/controllers/AuthoredBooksController.java
+++ src/main/java/com/springp/mongo_spring_prac/controllers/AuthoredBooksController.java
@@ -4,9 +4,7 @@ import com.springp.mongo_spring_prac.Services.AuthoredBookService;
 import com.springp.mongo_spring_prac.domain.AuthoredBook;
 import com.springp.mongo_spring_prac.domain.CreateUpdateBookRequest;
 import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @AllArgsConstructor
@@ -25,6 +23,35 @@ public class AuthoredBooksController {
                 // Convert the stream of books into a list of books.
                 .toList();
     }
+    @GetMapping(path="/{isbn}")
+    public ResponseEntity<AuthoredBookDto> getBookByIsbn(@PathVariable final String isbn) {
+        // Look in the database for a book with the provided ISBN
+        return authoredBookService.getBookByIsbn(isbn)
+                .map(book ->
+                        // Covert the found Book to a BookDTO
+                        ResponseEntity.ok(bookMapper.mapTo(book))
+                ).orElseGet(() ->
+                        // Or return HTTP 404 NOT FOUND
+                        new ResponseEntity<>(HttpStatus.NOT_FOUND)
+                );
+    }
+    @PutMapping(path = "/{isbn}")
+    public AuthoredBookDto createUpdateBook(
+            @PathVariable final String isbn,
+            @RequestBody final CreateUpdateBookRequestDto requestBody
+    ) {
+        // Convert from the create/update Book request presentation layer to service layer object.
+        final CreateUpdateBookRequest createUpdateBookRequest = createUpdateRequestMapper.mapFrom(requestBody);
 
+        // Pass the converted object to the service layer to create/update the book.
+        final AuthoredBook createUpdatedBook = authoredBookService.createUpdateBook(isbn, createUpdateBookRequest);
+
+        // Map the returned Book to a presentation layer object and return.
+        return bookMapper.mapTo(createUpdatedBook);
+    }
+//    @DeleteMapping(path = "/{isbn}")
+//    public void deleteBook(@PathVariable final String isbn) {
+//        authoredBookService.deleteBook(isbn);
+//    }
 
 }
